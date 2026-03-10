f2282ca6c1e360bb8be58f62d5c769c3d003b5ff
hgmuc <helmut.gassner.muc@gmail.com>
2026-03-10 14:58:44 +0100

    Fixing invalid paths to test data included in lvl2 folder of this test utility package.

--- src/rd_test_utils/fixtures/lvl2.py
+++ src/rd_test_utils/fixtures/lvl2.py
@@ -5,24 +5,24 @@ from pathlib import Path
 @pytest.fixture(scope="session")
 def lvl2_base_dir():
     BASE_DIR = Path(__file__).parent if "__file__" in globals() else Path.cwd()
-    return BASE_DIR / "data" / "lvl2"
+    return BASE_DIR / ".." / "data" / "lvl2"
 
 @pytest.fixture(scope="session")
 def lvl2_base_dir_str():
     BASE_DIR = Path(__file__).parent if "__file__" in globals() else Path.cwd()
-    return f"{BASE_DIR}/data/lvl2"
+    return f"{BASE_DIR}/../data/lvl2"
 
 @pytest.fixture(scope="session")
 def admin8_csv():
     BASE_DIR = Path(__file__).parent if "__file__" in globals() else Path.cwd()
-    return f"{BASE_DIR}/data/lvl2/admin-csv/admin8.csv"
+    return f"{BASE_DIR}/../data/lvl2/admin-csv/admin8.csv"
 
 @pytest.fixture(scope="session")
 def admin8_df():
     BASE_DIR = Path(__file__).parent if "__file__" in globals() else Path.cwd()
-    return pd.read_csv(f"{BASE_DIR}/data/lvl2/admin-csv/admin8.csv", sep="\t", index_col=0)
+    return pd.read_csv(f"{BASE_DIR}/../data/lvl2/admin-csv/admin8.csv", sep="\t", index_col=0)
 
 @pytest.fixture(scope="session")
 def add_features_shop():
     BASE_DIR = Path(__file__).parent if "__file__" in globals() else Path.cwd()
-    return f"{BASE_DIR}/data/lvl2/add-features-dach/shop/AM.txt"
+    return f"{BASE_DIR}/../data/lvl2/add-features-dach/shop/AM.txt"
