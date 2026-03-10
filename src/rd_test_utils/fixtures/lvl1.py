348428d13c0f975eff8318bce3660ed10b74534d
hgmuc <helmut.gassner.muc@gmail.com>
2026-03-10 14:57:05 +0100

    Added fixtures for loading large TagsetDict files ideally only once per session.

---
+++ src/rd_test_utils/fixtures/lvl1.py
@@ -0,0 +1,55 @@
+import pytest
+from pathlib import Path
+from basic_helpers.file_helper import do_ungzip_pkl, do_unpickle
+from lvl2_env_api.env_config import load_rev_tagset_dict
+
+@pytest.fixture(scope="session")
+def tagset_dict():
+    BASE_DIR = Path(__file__).parent if "__file__" in globals() else Path.cwd()
+    print("BASE_DIR - tagset_dict", str(BASE_DIR))
+    TAGSET_DICT_GZIP = BASE_DIR / ".." / "data" / "lvl1" / "TAGSET_DICT.gzip"
+    TAGSET_DICT_PKL = BASE_DIR / "lvl1" / "TAGSET_DICT.pkl"
+    try:
+        if TAGSET_DICT_GZIP.exists():
+            return do_ungzip_pkl(str(TAGSET_DICT_GZIP))
+        else:
+            return do_ungzip_pkl(str(TAGSET_DICT_PKL))
+    except Exception as e:
+        print("Could not load TAGSET_DICT_GZIP from", str(TAGSET_DICT_GZIP))
+        raise
+
+@pytest.fixture(scope="session")
+def rev_tagset_dict():
+    ways_base_path = f"C:/01_AnacondaProjects/osmium/lvl1"
+    return load_rev_tagset_dict(ways_base_path)
+
+
+@pytest.fixture(scope="session")
+def rev_tc_combi_dict():
+    BASE_DIR = Path(__file__).parent if "__file__" in globals() else Path.cwd()
+    REV_TC_PATH_GZIP = BASE_DIR / "lvl1" / "REV_TAGSET_COMBI_DICT.gzip"
+    REV_TC_PATH_PKL = BASE_DIR / "lvl1" / "REV_TAGSET_COMBI_DICT.pkl"
+    try:
+        if REV_TC_PATH_GZIP.exists():
+            return do_ungzip_pkl(str(REV_TC_PATH_GZIP))
+        else:
+            return do_ungzip_pkl(str(REV_TC_PATH_PKL))
+    except Exception as e:
+        print("Could not load REV_TAGSET_COMBI_DICT from", str(REV_TC_PATH_GZIP))
+        raise
+
+
+@pytest.fixture(scope="session")
+def rev_tc_combi_dict_prod():
+    BASE_DIR = Path("C:/01_AnacondaProjects/osmium")
+    REV_TC_PATH_GZIP = BASE_DIR / "lvl1" / "REV_TAGSET_COMBI_DICT.gzip"
+    REV_TC_PATH_PKL = BASE_DIR / "lvl1" / "REV_TAGSET_COMBI_DICT.pkl"
+    try:
+        if REV_TC_PATH_GZIP.exists():
+            return do_ungzip_pkl(str(REV_TC_PATH_GZIP))
+        else:
+            return do_ungzip_pkl(str(REV_TC_PATH_PKL))
+    except Exception as e:
+        print("Could not load REV_TAGSET_COMBI_DICT from", str(REV_TC_PATH_GZIP))
+        raise
+
